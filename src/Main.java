d894c4a38d977a0d38dc1b4cc151affa18f62405
Alexandre Nazareth <alexandrenazareth@hotmail.com>
2024-08-20 09:02:03 -0300

    -Exercicio 2

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("aula01");
+        frame.setContentPane(new Ex02().painelPrincipal);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(new Dimension(600, 900));
+        frame.setResizable(true);
+        frame.setVisible(true);
+
+    }
+}
