78d9f7d929ca07942fbcf4b37a98bc2453e97bdd
Alexandre Nazareth <alexandrenazareth@hotmail.com>
2024-08-20 12:18:44 -0300

    -Correções

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame("aula01");
+        JFrame frame = new JFrame("Exercicio 2");
         frame.setContentPane(new Ex02().painelPrincipal);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
